88a1043fc728ae189910f606f557061b72485647
BridgetKas <bridgetnamugga@gmail.com>
2024-08-29 19:14:33 +0900

    Added productivityApp

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,6 +1,10 @@
 import BoardComponent from "./boardContainer"
 import styles from './Board.module.css'
 import { useState } from "react"
+import Modal from "../modal/modalComponent"
+import { FaRegCircle } from "react-icons/fa";
+
+
 
 const boards = [
   {
@@ -23,13 +27,85 @@ const boards = [
 
 function Board() {
   const [board,setBoard] = useState(boards)
+  const [openModal,setOpenModal] = useState(false)
+  const [title,setTitle] = useState('')
+  const [status,setStatus] = useState('inComplete')
+  const [description,setDescription] = useState('')
+
+  function isModalOpen() {
+    setOpenModal(true)
+}
+
+function closeModal(){
+    setOpenModal(false)
+}
+
+function changeStatus(e) {
+    setStatus(e.target.value)
+}
+
+function saveTask() {
+    console.log('toffayo')
+}
+
   return (
-    <div className={styles.mainBoardContainer}>
-      {
-        board.map((item,index) =>(
-          <BoardComponent color={item.color} title={item.title} key={index}/>
-        ))
-      }
+    <div>
+      <div className={styles.features}>
+        <div className={styles.mainiconContainer}>
+          <div className={styles.iconContainer}>
+              <div className={styles.circleIcon}>
+                  <FaRegCircle  style={{color:'red'}}/>
+              </div>
+              <p>Task</p>
+          </div>
+          <div className={styles.iconContainer}>
+              <div className={styles.circleIcon}>
+                  <FaRegCircle  style={{color:'orange', fontSize:'18px'}}/>
+              </div>
+              <p>Story</p>
+          </div>
+          <div className={styles.iconContainer}>
+              <div className={styles.circleIcon}>
+                  <FaRegCircle  style={{color:'green'}}/>
+              </div>
+              <p>Bug</p>
+          </div>
+        </div>
+        <div className={styles.taskContainer}>
+          <div>
+              <button onClick={isModalOpen}>Add a Task</button>
+          </div>
+          <div className={styles.btnContainer}>
+              <button className={styles.button}>Add a Board</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.mainBoardContainer}>
+        {
+          board.map((item,index) =>(
+            <BoardComponent color={item.color} title={item.title} key={index}/>
+          ))
+        }
+      <Modal show={openModal} onClose={closeModal} status={status}>
+        <div >
+          <div className={styles.textareaContainer}>
+              <input type='text' placeholder='Enter a title...' value={title} className={styles.taskInput} onChange={(e) => setTitle(e.target.value)}/>
+              <textarea id="story" rows="5" cols="33" placeholder='Enter a description' className={styles.textarea} value={description} onChange={(e) => setDescription(e.target.value)}> </textarea>
+          </div>
+          <div>
+            <select className={styles.selectarea}  value={status} onChange={changeStatus}>
+              <option value="inComplete" >Incomplete</option>
+              <option value="inprogress">In progress</option>
+              <option value="reviewing">Reviewing</option>
+              <option value="complete">Complete</option>
+            </select>
+          </div>
+          <div className='modalContainer'>
+              <button className={styles.saveBtn} onClick={saveTask} disabled={!title || !description}>Save</button>
+          </div>
+        </div>
+      </Modal>
+    </div>
     </div>
   )
 }
--- src/Components/topBar/topBar.jsx
+++ src/Components/topBar/topBar.jsx
@@ -1,30 +1,11 @@
 import { IoSearch } from "react-icons/io5";
 import styles from './topBar.module.css'
 import { RiBearSmileFill } from "react-icons/ri";
-import { FaRegCircle } from "react-icons/fa";
-import {useState} from "react"
-import Modal from "../modal/modalComponent"
 
 
 function TopBar() {
 
-    const [openModal,setOpenModal] = useState(false)
-    const [title,setTitle] = useState('')
-    const [status,setStatus] = useState('inComplete')
-    
-
-    function isModalOpen() {
-        setOpenModal(true)
-    }
-
-    function closeModal(){
-        setOpenModal(false)
-    }
-    
-    function changeStatus(e) {
-        setStatus(e.target.value)
-    }
-
+   
   return (
     <header>
         <div className={styles.topBarContainer}>
@@ -45,58 +26,7 @@ function TopBar() {
                 </div>
             </div>
         </div>
-        <div>
-            <div className={styles.features}>
-                <div className={styles.mainiconContainer}>
-                    <div className={styles.iconContainer}>
-                        <div className={styles.circleIcon}>
-                            <FaRegCircle  style={{color:'red'}}/>
-                        </div>
-                        <p>Task</p>
-                    </div>
-                    <div className={styles.iconContainer}>
-                        <div className={styles.circleIcon}>
-                            <FaRegCircle  style={{color:'orange', fontSize:'18px'}}/>
-                        </div>
-                        <p>Story</p>
-                    </div>
-                    <div className={styles.iconContainer}>
-                        <div className={styles.circleIcon}>
-                            <FaRegCircle  style={{color:'green'}}/>
-                        </div>
-                        <p>Bug</p>
-                    </div>
-                </div>
-                <div className={styles.taskContainer}>
-                    <div>
-                        <button onClick={isModalOpen}>Add a Task</button>
-                    </div>
-                    <div className={styles.btnContainer}>
-                        <button className={styles.button}>Add a Board</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <Modal show={openModal} onClose={closeModal} status={status}>
-            <div >
-                <div className={styles.textareaContainer}>
-                    <input type='text' placeholder='Enter a title...' value={title} className={styles.taskInput} onChange={(e) => setTitle(e.target.value)}/>
-                    <textarea id="story" rows="5" cols="33" placeholder='Enter a description' className={styles.textarea}> </textarea>
-                </div>
-                <div>
-                    {/* <label style={{color:'black'}}>Choose a task status</label> */}
-                    <select className={styles.selectarea}  value={status} onChange={changeStatus}>
-                        <option value="inComplete" selected>Incomplete</option>
-                        <option value="inprogress">In progress</option>
-                        <option value="reviewing">Reviewing</option>
-                        <option value="complete">Complete</option>
-                    </select>
-                </div>
-                <div className='modalContainer'>
-                    <button className={styles.saveBtn} onClick=''>Save</button>
-                </div>
-            </div>
-       </Modal>
+        
     </header>
   )
 }
